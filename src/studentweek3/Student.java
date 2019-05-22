44f16bdf1b64c8de66f99546b9f9d8a9aef667fd
kaura <kaura@10.48.1.8>
2019-05-22 09:40:48 -0400

    initial commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,44 @@
+/*
+ * Arshdeep kaur
+ * Student Id: 991545648
+ * SYST10199 - Web Programming
+ */
+package studentweek3;
+
+/**
+ *
+ * @author kaura
+ */
+public class Student {
+    
+    private int id;
+    private String firstName;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ src/studentweek3/Studentweek3.java
@@ -0,0 +1,25 @@
+/*
+ * Arshdeep kaur
+ * Student Id: 991545648
+ * SYST10199 - Web Programming
+ */
+package studentweek3;
+
+/**
+ *
+ * @author kaura
+ */
+public class Studentweek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student();
+        s1.setFirstName("john");
+        s1.setId(4);
+    }
+    
+}
+
